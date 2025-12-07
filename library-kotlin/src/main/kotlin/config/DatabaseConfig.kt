b0fbf42dc8c2529d13b8149cebe123f01b2db0f9
LuckHost <sh76484@gmail.com>
2025-12-07 15:08:50 +0700

    убрал логирование для лабы 5

--- library-kotlin/src/main/kotlin/config/DatabaseConfig.kt
+++
@@ -1,6 +0,0 @@
-package edu.sfu.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-
-open class DatabaseConfig
